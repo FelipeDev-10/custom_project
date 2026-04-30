d962f6c95d79e71600d653c36a8776c4b6c43527
FelipeDev-10 <felipessant07@gmail.com>
2026-04-30 20:20:51 -0300

    Plano de fundo e Icons Transparentes

--- App.js
+++ App.js
@@ -1,55 +1,130 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, ImageBackground} from 'react-native';
 
 export default function App() {
+
+  const dados = [
+    {
+      id: '1',
+      titulo: 'Cariacica',
+      descricao: 'Clima: Ensolarado',
+      imagem: require('./assets/sol.png'),
+    },
+    {
+      id: '2',
+      titulo: 'Vila Velha',
+      descricao: 'Clima: Chuvoso',
+      imagem: require('./assets/chuva sem fundo.png'),
+    },
+    {
+      id: '3',
+      titulo: 'Vitória',
+      descricao: 'Clima: Chuvoso',
+      imagem: require('./assets/chuva sem fundo.png'),
+    },
+    {
+      id: '4',
+      titulo: 'Viana',
+      descricao: 'Clima: Ensolarado',
+      imagem: require('./assets/sol.png'),
+    }
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={styles.card}>
+
+      <Image
+        source={item.imagem}
+        style={styles.imagem}
+      />
+
+      <View style={styles.textoContainer}>
+        <Text style={styles.titulo}>{item.titulo}</Text>
+        <Text style={styles.descricao}>{item.descricao}</Text>
+      </View>
+
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-    <Image
-      source={require('C:/projeto_custom/projeto_custom/assets/sol.png')}
-      style={{
-        width: 80,
-        height: 80,
-        }}
-    />
 
-    <Image
-      source={require('C:/projeto_custom/projeto_custom/assets/sol entre nuvens.png')}
-      style={{
-        width: 80,
-        height: 80,
-        }}
-    />
-    
-    <Image
-      source={require('C:/projeto_custom/projeto_custom/assets/sol com chuva.png')}
-      style={{
-        width: 100,
-        height: 100,
-        }}
-    />
+    <ImageBackground
+    source={require('./assets/background.png')}
+    style={{ flex: 1 }}
+    resizeMode="cover"
+  >
 
-    <Image
-      source={require('C:/projeto_custom/projeto_custom/assets/sol.png')}
-      style={{
-        width: 80,
-        height: 80,
-        }}
-    />
+    <View style={{ flex: 1, padding: 10 }}>
+
+      <View style={styles.tituloPrincipal}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+          App Previsão do Tempo
+        </Text>
+      </View>
+
+      <FlatList
+        data={dados}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+      />
+
+      <StatusBar style="auto" />
 
     </View>
 
+  </ImageBackground>
 
 
-
+    </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#14cbc5',
+    backgroundColor: 'transparent',
+    padding: 0,
+  },
+
+  card: {
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    padding: 12,
+    marginBottom: 12,
+    borderRadius: 8,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  imagem: {
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+    marginRight: 10,
+    backgroundColor: 'transparent',
+  },
+
+  textoContainer: {
+    flex: 1,
+  },
+
+  tituloPrincipal: {
+    alignItems: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 50,
+    marginBottom: 50,
+  },
+
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 4,
+  },
+
+  descricao: {
+    fontWeight: 'bold',
+    color: '#ffffff',
   },
 });
